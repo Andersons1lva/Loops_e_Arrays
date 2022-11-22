b177c1da1235be2dae0ff99715835de6a8c85a7c
andersons1lva <andersons1lva@yahoo.com.br>
2022-11-21 21:32:15 -0300

    Exercicios de loops e arrays

---
+++ src/br/com/dio/exercicios/loops/Ex1_NomeEIdade.java
@@ -0,0 +1,26 @@
+package br.com.dio.exercicios.loops;
+
+/* Faça um programa que leia conjuntos de dois valores,
+o primeiro representantando o nome do aluno e o segundo representando a sua idade.
+(Pare o programa inserindo o valor 0 no campo nome)
+ */
+
+import java.util.Scanner;
+
+public class Ex1_NomeEIdade {
+    public static void main(String[] args) {
+        // instancia da classe Scanner para leitura do teclado.
+        Scanner scan = new Scanner(System.in);
+
+        String nome;
+        int idade;
+
+        while (true) {
+            System.out.println("Nome: ");
+            nome = scan.next();
+            if (nome.equals("0")) break;
+            System.out.println("Idade: ");
+            idade = scan.nextInt();
+        }
+    }
+}

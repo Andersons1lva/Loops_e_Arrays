bf9093733dbf207f5d58a7ef9bac5fdb436adb36
andersons1lva <andersons1lva@yahoo.com.br>
2022-11-24 22:07:28 -0300

    Exercicios de loops e arrays

--- src/br/com/dio/exercicios/loops/Ex2_Nota.java
+++ src/br/com/dio/exercicios/loops/Ex2_Nota.java
@@ -6,6 +6,20 @@ package br.com.dio.exercicios.loops;
 * até que o usuário informe uma valor válido.
 * */
 
-public class Ex2_Nota {
+import java.util.Scanner;
 
+public class Ex2_Nota {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int nota;
+
+        System.out.println("Nota: ");
+        nota = scan.nextInt();
+
+        while (nota < 0 || nota > 10){
+            System.out.println("Nota Inválida! Digite Novamente: ");
+            nota = scan.nextInt();
+        }
+    }
 }

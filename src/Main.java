b5d007bb0a9cab3d65b77ed6c97ad7326acfb8c7
andersons1lva <andersons1lva@yahoo.com.br>
2022-11-30 16:42:05 -0300

    Exercicios de Metodos.(Ex Quadrilatero).

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import br.com.dio.exercicios.Metodos.Emprestimo;
 import br.com.dio.exercicios.Metodos.Calculadora;
 import br.com.dio.exercicios.Metodos.Mensagem;
+import br.com.dio.exercicios.Sobrecarga.Quadrilatero;
 
 public class Main {
     public static void main(String[] args) {
@@ -22,5 +23,11 @@ public class Main {
         Emprestimo.calcular(1000, Emprestimo.getDuasParcelas());
         Emprestimo.calcular(2000, Emprestimo.getTresParcelas());
         Emprestimo.calcular(3000, 3);
+
+        System.out.println("Quadrilatero");
+        Quadrilatero.area(3);
+        Quadrilatero.area(2d,4d);
+        Quadrilatero.area(3,5,7);
+        Quadrilatero.area(3f,6f);
     }
 }
---
+++ src/br/com/dio/exercicios/Sobrecarga/Quadrilatero.java
@@ -0,0 +1,24 @@
+package br.com.dio.exercicios.Sobrecarga;
+
+/**
+ * Classe de exemplo para o exercício da Aula 2 de Métodos.
+ */
+
+public class Quadrilatero {
+
+    // Sobrecarga do Método acontece quando o nome do Método são mantidos, porém, seus parametros e o tipos dos parametros são alterados.
+
+    public static void area(double lado){
+        System.out.println("Área do quadrado: " + lado * lado);
+    }
+    public static void area(double lado1, double lado2){
+        System.out.println("Área do retângulo: " + lado1 * lado2);
+    }
+    public static void area(double baseMaior, double baseMenor, double altura){
+        System.out.println("Área do trapézio:" + ((baseMaior + baseMenor)* altura) / 2);
+    }
+
+    public static void area(float diagonal1, float diagonal2){
+        System.out.println("Área do losango: " + (diagonal1 * diagonal2)/2);
+    }
+}
